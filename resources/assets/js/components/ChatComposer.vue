f58e13c707358df990365b180f73e748be23a960
alexleong527 <alexleong@Alexde-MacBook-Pro.local>
2017-08-27 15:10:09 +0800

    creat chat composer

---
+++ resources/assets/js/components/ChatComposer.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <div class="chat-composer">
+      <input type="text" placeholder="typing your message...">
+      <button type="button" class="btn btn-primary">Send</button>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+  .chat-composer {
+    display: flex;
+  }
+
+  .chat-composer input {
+    flex: 1 auto;
+  }
+
+  .chat-composer button {
+    border-radius: 0;
+  }
+</style>

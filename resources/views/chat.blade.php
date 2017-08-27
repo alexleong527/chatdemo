ab0dc7e3ea125e299abbd3973be8265d06cbfcad
alexleong527 <alexleong@Alexde-MacBook-Pro.local>
2017-08-27 14:21:59 +0800

    add chatroom page

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chatroom</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <h1>Chatroom</h1>
+    
+    <script src="js/app.js"></script>
+  </body>
+</html>
